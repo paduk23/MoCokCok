df31ed6669372bf6b400a0bf89adeb472aa63c85
Paduk <bov00@naver.com>
2021-12-07 00:46:26 +0900

    set viewmodel

---
+++ MoCokCok/Views/LoginView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using MoCokCok.ViewModels;
+using System.Windows;
+
+namespace MoCokCok.Views
+{
+    /// <summary>
+    /// LoginView.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class LoginView : Window
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+            DataContext = new LoginViewModel();
+        }
+    }
+}
