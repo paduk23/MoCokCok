5c7b9eb6bca96a42ea7f21092dfef3b74d375023
Paduk <bov00@naver.com>
2021-12-07 00:45:52 +0900

    Avoid duplication process

--- MoCokCok/App.xaml.cs
+++ MoCokCok/App.xaml.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using MoCokCok.ViewModels;
+using MoCokCok.Views;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +16,32 @@ namespace MoCokCok
     /// </summary>
     public partial class App : Application
     {
+        private const string mutexName = "MoCokCok_Project";
+        private Mutex _mutex = null;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            try
+            {
+                _mutex = new Mutex(true, mutexName, out bool isNew);
+                if (isNew)
+                {
+                    base.OnStartup(e);
+                    var login = new LoginView();
+                    login.ShowDialog();
+                }
+                else
+                {
+                    MessageBox.Show($"MoCokCok는 이미 실행중입니다.", $"중복 실행", MessageBoxButton.OK, MessageBoxImage.Information);
+                    Current.Shutdown();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + "\n\n" + ex.StackTrace + "\n\n" + "Application Existing...", "Exception thrown");
+                Current.Shutdown();
+            }
+        }
+
     }
 }
